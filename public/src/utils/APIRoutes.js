ef8f65ace207ef6c8e623fdd5fbccbb1652d833c
Rafik-Antonyan <rafikantonyan105@gmail.com>
2023-09-01 16:56:19 +0400

    15

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chat-emoji-barl5jf40-rafik-antonyan.vercel.app"
+export const host = "https://chat-emoji-9e20dpruj-rafik-antonyan.vercel.app"
 export const registrationRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const setAvatarRoute = `${host}/api/auth/setAvatar`
