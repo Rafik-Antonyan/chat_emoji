479cf1252d7f6cf80d58f8c7e037311cc01fd159
Rafik-Antonyan <rafikantonyan105@gmail.com>
2023-09-01 14:43:17 +0400

    fix cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,9 @@ const socket = require('socket.io')
 
 require("dotenv").config()
 
-app.use(cors())
+app.use(cors({
+    origin: "*"
+}))
 app.use(express.json())
 
 app.use('/api/auth', userRoutes)
